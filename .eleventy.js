614724328211de2bc49829939e8999c148f93d5f
Jordi Marimon <jordimarimon7@gmail.com>
2022-07-10 17:59:57 +0200

    docs: add css to 11ty

--- .eleventy.js
+++ .eleventy.js
@@ -1,11 +1,14 @@
-module.exports = function (/* eleventyConfig */) {
+module.exports = function (eleventyConfig) {
+    eleventyConfig.addPassthroughCopy('docs/assets');
+
     return {
         dir: {
             input: 'docs',
             output: '_site',
             includes: '_includes',
             layouts: '_layouts',
-            templateFormats: ['njk', 'md', 'html'],
         },
+        templateFormats: ['njk', 'md', 'html'],
+        pathPrefix: process.env.NODE_ENV === 'production' ? '/ts-ast-parser/' : '/',
     };
 };
