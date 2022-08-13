dcaf332fc5f281e55835c1865da1f3c6f5b52f52
Jordi Marimon <jordimarimon7@gmail.com>
2022-08-13 12:44:18 +0200

    docs: add sitemap.xml, robots.txt and syntax highlighting

--- .eleventy.js
+++ .eleventy.js
@@ -1,11 +1,25 @@
+const syntaxHighlight = require('@11ty/eleventy-plugin-syntaxhighlight');
+const {DateTime} = require('luxon');
+
 module.exports = function (eleventyConfig) {
 
+    // Adds the following files in the bundle
     eleventyConfig.addPassthroughCopy('docs/favicon.ico');
+    eleventyConfig.addPassthroughCopy({'docs/robots.txt': '/robots.txt'});
 
+    // Make sure the nav items are ordered correctly
     eleventyConfig.addCollection('page', (collections) => {
         return collections.getFilteredByTag('page').sort((a, b) => {
             return a.data.order - b.data.order;
         });
+    });
+
+    // Syntax highlighting: https://www.11ty.dev/docs/plugins/syntaxhighlight/
+    eleventyConfig.addPlugin(syntaxHighlight);
+
+    // For the sitemap
+    eleventyConfig.addShortcode('currentDate', (date = DateTime.now()) => {
+        return date;
     });
 
     return {
