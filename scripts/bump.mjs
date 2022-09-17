341426cf3302e8b4d6ba91fe31e5c02a9cd706d0
Jordi Marimon <jordimarimon7@gmail.com>
2022-09-17 18:20:05 +0200

    chore: add script to bump workspace packages versions

---
+++ scripts/bump.mjs
@@ -0,0 +1,53 @@
+import { globbySync } from 'globby';
+import chalk from 'chalk';
+import path from 'path';
+import fs from 'fs';
+
+const mainPkg = JSON.parse(fs.readFileSync(path.join(process.cwd(), 'package.json'), 'utf8'));
+const packages = globbySync(path.join('packages/*', 'package.json'));
+
+for (const pkgPath of packages) {
+    const pkg = JSON.parse(fs.readFileSync(pkgPath, 'utf8'));
+
+    console.log(chalk.blue(`Checking package ${pkg.name}`));
+
+    let hasChanged = false;
+
+    if (pkg?.dependencies) {
+        hasChanged = update(pkg.dependencies);
+    }
+
+    if (pkg?.peerDependencies) {
+        hasChanged = update(pkg.peerDependencies) || hasChanged;
+    }
+
+    if (pkg?.optionalDependencies) {
+        hasChanged = update(pkg.optionalDependencies) || hasChanged;
+    }
+
+    if (hasChanged) {
+        console.log(chalk.red(`\tWriting new package.json`));
+
+        fs.writeFileSync(pkgPath, JSON.stringify(pkg, null, 4) + '\n', 'utf8');
+    }
+}
+
+function update(dependencies) {
+    let hasChanged = false;
+
+    const isWorkspacePkg = (name) => name.startsWith('@ts-ast-parser');
+
+    for (const [pkg, oldVersion] of Object.entries(dependencies)) {
+        const newVersion = isWorkspacePkg(pkg)
+            ? oldVersion
+            : (mainPkg.dependencies?.[pkg] || mainPkg.optionalDependencies?.[pkg] || mainPkg.devDependencies?.[pkg]);
+
+        if (oldVersion !== newVersion) {
+            dependencies[pkg] = newVersion;
+            console.log(chalk.green(`\tUpdating ${pkg} from ${oldVersion} to ${newVersion}`));
+            hasChanged = true;
+        }
+    }
+
+    return hasChanged;
+}
