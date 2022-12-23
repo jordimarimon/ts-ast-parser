e617e86c84296d7f85c55337c84ece482da8c6b5
Jordi Marimon <jordimarimon7@gmail.com>
2022-12-23 21:43:54 +0100

    chore: limit to 100 characters commit subject and body line

--- commitlint.config.cjs
+++ commitlint.config.cjs
@@ -1,5 +1,7 @@
 module.exports = {
-    extends: ['@commitlint/config-conventional'],
+    extends: [
+        '@commitlint/config-conventional',
+    ],
     rules: {
         'header-max-length': [2, 'always', 100],
         'body-leading-blank': [2, 'always'],
@@ -7,5 +9,11 @@ module.exports = {
         'body-max-length': [0, 'always'],
         'footer-leading-blank': [2, 'always'],
         'footer-max-line-length': [2, 'always', 100],
+        'scope-empty': [0, 'never'],
+        'scope-enum': [2, 'always', [
+            'core',
+            'readers',
+            'deps',
+        ]],
     },
 };
